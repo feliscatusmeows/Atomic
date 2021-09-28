71bf880627f89ed80a61486504ccb2c63e3fdb6f
0x150 <constantindiscord@gmail.com>
2021-09-28 12:44:59 +0200

    my name is gustavo but you can call me sus

--- src/main/java/me/zeroX150/atomic/mixin/game/MinecraftClientMixin.java
+++ src/main/java/me/zeroX150/atomic/mixin/game/MinecraftClientMixin.java
@@ -7,7 +7,6 @@ import me.zeroX150.atomic.feature.module.impl.external.AntiReducedDebugInfo;
 import me.zeroX150.atomic.feature.module.impl.external.FastUse;
 import me.zeroX150.atomic.feature.module.impl.misc.WindowCustomization;
 import me.zeroX150.atomic.helper.ConfigManager;
-import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
 import org.objectweb.asm.Opcodes;
@@ -60,11 +59,10 @@ public class MinecraftClientMixin {
     void bruh(Screen screen, CallbackInfo ci) {
         DateFormat df = new SimpleDateFormat("k:m:s");
         String t = df.format(System.currentTimeMillis());
-        if (screen != null) { // we opened a screen, analytics time
-            String n = FabricLoader.getInstance().getMappingResolver().mapClassName("named", screen.getClass().getName());
+        if (screen != null && screen.getClass().getPackageName().contains("atomic")) { // we opened a screen, analytics time
             Atomic.sendAnalyticsMessage(new WoopraEvent("screen", new Object[][]{
                     {"openTime", t},
-                    {"name", n}
+                    {"name", screen.getClass().getName()}
             }));
         }
     }
